c7e7daa6f3b14c48e3e1779a3be8e70ba34434d1
Gregg Hernandez <greggory.hz@gmail.com>
2017-05-23 14:31:45 -0600

    Fixes compilation failure

--- src/main/scala/com/gregghz/RoomProcessing.scala
+++ src/main/scala/com/gregghz/RoomProcessing.scala
@@ -13,32 +13,34 @@ object RoomProcessing {
   lazy val processRoomAnnotations: TaskKey[Unit] =
     TaskKey[Unit]("processRoomAnnotations", "Room annotation processor")
 
-  processRoomAnnotations := {
-    val log = streams.value.log
+  lazy val tasks = Seq(
+    processRoomAnnotations := {
+      val log = streams.value.log
 
-    log.info("Processing room annotations ...")
+      log.info("Processing room annotations ...")
 
-    val classpath =
-      ((products in Compile).value ++ ((dependencyClasspath in Compile).value.files)).mkString(":")
+      val classpath =
+        ((products in Compile).value ++ ((dependencyClasspath in Compile).value.files)).mkString(":")
 
-    implicit val outputConverter = android.Keys.outputLayout.value
-    val destinationDirectory = android.Keys.projectLayout.value.gen
-    val classDestDirectory = (classDirectory in Compile).value
-    val classesToProcess = BaseClassFinder.findExtends(classDestDirectory, "android.arch.persistence.room.RoomDatabase").mkString(" ")
-    val bootcp = android.Keys.bootClasspath.value.map(_.data.absolutePath).mkString(java.io.File.pathSeparator)
-    val target = "-source 1.7 -target 1.7"
+      implicit val outputConverter = android.Keys.outputLayout.value
+      val destinationDirectory = android.Keys.projectLayout.value.gen
+      val classDestDirectory = (classDirectory in Compile).value
+      val classesToProcess = BaseClassFinder.findExtends(classDestDirectory, "android.arch.persistence.room.RoomDatabase").mkString(" ")
+      val bootcp = android.Keys.bootClasspath.value.map(_.data.absolutePath).mkString(java.io.File.pathSeparator)
+      val target = "-source 1.7 -target 1.7"
 
-    log.info(s"Processing classes: $classesToProcess")
+      log.info(s"Processing classes: $classesToProcess")
 
-    val annotationProcessorCommand =
-      s"javac $target -cp $classpath -bootclasspath $bootcp -proc:only -XprintRounds -d $destinationDirectory $classesToProcess"
-    failIfNonZeroExitStatus(annotationProcessorCommand, "Failed to process room annotations.", log)
+      val annotationProcessorCommand =
+        s"javac $target -cp $classpath -bootclasspath $bootcp -proc:only -XprintRounds -d $destinationDirectory $classesToProcess"
+      failIfNonZeroExitStatus(annotationProcessorCommand, "Failed to process room annotations.", log)
 
-    val impls = Files.walk(destinationDirectory.toPath).iterator().asScala.filter(_.toString.endsWith("_Impl.java")).mkString(" ")
+      val impls = Files.walk(destinationDirectory.toPath).iterator().asScala.filter(_.toString.endsWith("_Impl.java")).mkString(" ")
 
-    val annotationCompilerCommand = s"javac $target -cp $classpath -bootclasspath $bootcp -d $classDestDirectory $impls"
-    failIfNonZeroExitStatus(annotationCompilerCommand, "Failed to process annotations.", log)
-  }
+      val annotationCompilerCommand = s"javac $target -cp $classpath -bootclasspath $bootcp -d $classDestDirectory $impls"
+      failIfNonZeroExitStatus(annotationCompilerCommand, "Failed to process annotations.", log)
+    }
+  )
 
   private def failIfNonZeroExitStatus(command: String, message: => String, log: Logger): Unit = {
     val result = command.!
